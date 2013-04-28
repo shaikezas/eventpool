7c4fb2932207df3605d6549165ffada0b6ed0dfa
ramu <ramuenugurthi@gmail.com>
2013-04-28 15:57:35 +0530

    added eventdto save with title

--- src/main/java/com/eventpool/web/controller/EventServiceImpl.java
+++ src/main/java/com/eventpool/web/controller/EventServiceImpl.java
@@ -1,6 +1,7 @@
 package com.eventpool.web.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -9,9 +10,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import com.eventpool.common.dto.AddressDTO;
 import com.eventpool.common.dto.EventDTO;
+import com.eventpool.common.dto.TicketDTO;
 import com.eventpool.common.entities.Event;
 import com.eventpool.common.repositories.EventRepository;
+import com.eventpool.common.type.EventInfoType;
+import com.eventpool.common.type.EventStatus;
+import com.eventpool.common.type.EventType;
 import com.eventpool.event.module.EventApi;
 import com.eventpool.event.module.EventMapper;
 
@@ -40,7 +46,53 @@ public class EventServiceImpl implements EventService {
     public void addEvent(EventDTO event) {
     	eventList.add(event);
     	id++;
-    	//eventApi.saveEventDTO(event);
+    	
+    	event.setContactDetails("contact details");
+    	event.setCreatedBy(2L);
+    	event.setCreatedDate(new Date());
+    	event.setDescription("description");
+    	event.setEndDate(new Date());
+    	event.setEventType(EventType.FREE);
+    	event.setInfoType(EventInfoType.BASIC);
+    	event.setIsActive(true);
+    	event.setModifiedDate(new Date());
+    	event.setOrganizerDescription("org desc");
+    	event.setOrganizerName("orgname");
+    	event.setStartDate(new Date());
+    	event.setStatus(EventStatus.OPEN);
+    	event.setSubCategoryId(1);
+    	event.setTermsAndConditions("terms and conditions");
+    	AddressDTO venueAddress = new AddressDTO();
+    	venueAddress.setAddress1("adr1");
+    	venueAddress.setAddress2("addr2");
+    	venueAddress.setCityId(1);
+    	venueAddress.setFax("fax");
+    	venueAddress.setMapUrl("mapUrl");
+    	venueAddress.setMobileNumber("molenumbe");
+    	venueAddress.setPhoneNumber("ph nber");
+    	venueAddress.setZipCode(12345L);
+    	event.setVenueAddress(venueAddress);
+    	event.setVenueName("venue name");
+    	
+    	TicketDTO ticket = new TicketDTO();
+    	ticket.setCreatedBy(0L);
+    	ticket.setCreatedDate(new Date());
+    	ticket.setDescription("descriptions");
+    	ticket.setIsActive(true);
+    	ticket.setMaxQty(10);
+    	ticket.setMinQty(2);
+    	ticket.setModifiedDate(new Date());
+    	ticket.setName("name");
+    	ticket.setPrice(2.3d);
+    	ticket.setQuantity(100);
+    	ticket.setSaleEnd(new Date());
+    	ticket.setSaleStart(new Date());
+
+    	List<TicketDTO> tickets = new ArrayList<TicketDTO>();
+    	tickets.add(ticket);
+    	event.setTickets(tickets);
+
+    	eventApi.saveEventDTO(event);
     }
 
     public void deleteEventById(Long id) {
