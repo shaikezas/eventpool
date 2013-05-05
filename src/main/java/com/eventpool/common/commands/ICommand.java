eab69f0228d94790d90015c9fe3cbba33e3058d9
shaikezas <shaikezas@gmail.com>
2013-05-05 23:54:53 +0530

    fixed compilation issues

--- src/main/java/com/eventpool/common/commands/ICommand.java
+++ src/main/java/com/eventpool/common/commands/ICommand.java
@@ -5,10 +5,12 @@ import javax.xml.bind.annotation.XmlSeeAlso;
 
 import com.eventpool.event.command.SaveEventCommand;
 import com.eventpool.ticket.commands.TicketBlockedCommand;
+import com.eventpool.ticket.commands.TicketCreatedCommand;
 import com.eventpool.ticket.commands.TicketOrderedCommand;
 import com.eventpool.ticket.commands.TicketUnBlockedCommand;
+import com.eventpool.ticket.commands.TicketUpdatedCommand;
 
-@XmlSeeAlso({TicketOrderedCommand.class,TicketBlockedCommand.class,TicketUnBlockedCommand.class,SaveEventCommand.class})
+@XmlSeeAlso({TicketOrderedCommand.class,TicketBlockedCommand.class,TicketUnBlockedCommand.class,SaveEventCommand.class,TicketCreatedCommand.class,TicketUpdatedCommand.class})
 //@XmlRootElement
 public interface ICommand {
 
