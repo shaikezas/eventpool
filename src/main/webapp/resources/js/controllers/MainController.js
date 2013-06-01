e698d3fdd6f0d0797f262ca5880b402aa92e6dbf
shaikezas <shaikezas@gmail.com>
2013-06-01 19:55:46 +0530

    header selection fix.

--- src/main/webapp/resources/js/controllers/MainController.js
+++ src/main/webapp/resources/js/controllers/MainController.js
@@ -1,5 +1,10 @@
-function MainController($scope, $route, $routeParams) {
+function MainController($scope, $route, $routeParams,$location) {
 	$scope.title = "Home";
 	$scope.header = "home";
-	
+	 $scope.path = function () {
+         return $location.url();
+     };
+     $scope.activeWhen = function (value) {
+         return value ? 'active' : '';
+     };
 }
