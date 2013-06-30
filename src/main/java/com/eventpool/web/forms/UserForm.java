6891eb03ce659cf1ebf2f227cb109778e12c2419
shaikezas <shaikezas@gmail.com>
2013-06-30 21:30:57 +0530

    Created UserForm.

--- src/main/java/com/eventpool/web/forms/UserForm.java
+++ src/main/java/com/eventpool/web/forms/UserForm.java
@@ -1,4 +1,4 @@
-package com.eventpool.common.entities;
+package com.eventpool.web.forms;
 
 import java.util.Date;
 
@@ -14,114 +14,48 @@ import org.springframework.security.core.GrantedAuthority;
 
 import com.eventpool.common.type.Gender;
 
-@Entity
-@Table(name="USER", uniqueConstraints = {@UniqueConstraint (columnNames = "USERNAME"),      @UniqueConstraint (columnNames = "EMAIL")})
-public class User extends IdEntity{
+public class UserForm {
 	
-	private static final String SALT = "cewuiqwzie"; // This is required for password change through Spring security
 	
-	public User(){
-		
-	}
 	
-	public User(String userName){
-		
-		this.userName = userName;
-		
-	}
-	public User(String userName, String password) {
-	    // By default account enabled should be false, once the user clicks on confirmation email, this flag should be set
-	    // to true
-	    accountExpired = false;
-
-	    this.userName = userName;
-	    this.password = encode(password);
-	  }
-	 public String encode(String password) {
-		    return new Md5PasswordEncoder().encodePassword(password, SALT);
-	 }
-	 public void setTemporaryPassword(String password) {
-		  this.password = encode(password);
-	  }
-	 public Boolean Authenticate(String password) {
-		    if (this.password.equals(encode(password))) {
-		      return true;
-		    }
-		    System.out.println("Original password is: " + this.password);
-		    System.out.println("Password entered  is: " + encode(password));
-		    return false;
-		  }
-	 public void updatePassword(String old, String newPass1, String newPass2) {
-		    if (!password.equals(old)) {
-		      throw new IllegalArgumentException("Existing Password invalid");
-		    }
-		    if (!newPass1.equals(newPass2)) {
-		      throw new IllegalArgumentException("New Passwords don't match");
-		    }
-		    password = encode(newPass1);
-	}
 
 	
-	@Column(name = "FNAME")
 	private String fname;
 	
-	@Column(name = "ACCOUNT_ENABLED")
 	  boolean accountEnabled =true; // This would depend upon business requirements
 
-	@Column(name = "ACCOUNT_EXPIRED")
 	  boolean accountExpired = false;
 	
-	 
-	@Column(name = "LNAME")
 	private String lname;
 	
-	@Column(name = "USERNAME")
-	@NotNull
 	private String userName;
 	
-	@Column(name="PASSWORD")
-	@NotNull
 	private String password;
 
 	
-	@Column(name = "EMAIL")
 	private String email;
 
-	@Column(name = "COMPANY")
 	private String company;
 
-	@Column(name = "PHONE")
 	private String phone;
 
-	@Column(name = "MOBILE")
 	private String mobile;
 
-	@Column(name = "ALT_EMAIL")
 	private String altEmail;
 
-	@Column(name = "HOME_ADDRESS")
 	private String homeAddress;
 
-	@Column(name = "OFFICE_ADDRESS")
 	private String officeAddress;
 
-	@Column(name = "SHIPPING_ADDRESS")
 	private String shippingAddress;
 
-	@Column(name = "GENDER")
 	private Gender geneder;
 
-	@Column(name = "DOB")
 	private Date dob;
 
-	@Column(name = "COMPANY_URL")
 	private String companyUrl;
 
-	@Column(name = "CREATED_DATE", nullable = false)
-	private Date createdDate = new Date();
-
-	@Column(name = "MODIFIED_DATE", nullable = false)
-	private Date modifiedDate = new Date();
+	private String createdDate ;
 
 	public String getFname() {
 		return fname;
@@ -227,21 +161,6 @@ public class User extends IdEntity{
 		this.companyUrl = companyUrl;
 	}
 
-	public Date getCreatedDate() {
-		return createdDate;
-	}
-
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
-
-	public Date getModifiedDate() {
-		return modifiedDate;
-	}
-
-	public void setModifiedDate(Date modifiedDate) {
-		this.modifiedDate = modifiedDate;
-	}
 
 	public Gender getGeneder() {
 		return geneder;
@@ -259,16 +178,6 @@ public class User extends IdEntity{
 		    }
 		  }
 	  
-	  @Transient
-	  private Roles[] roles = new Roles[]{Roles.ROLE_USER}; // Roles of this user -- This is required for Spring Security
-	  
-	  public Roles[] getRoles() {
-		    return roles;
-		  }
-
-		  public void setRoles(Roles... roles) {
-		    this.roles = roles;
-		  }
 		public boolean isAccountEnabled() {
 			return accountEnabled;
 		}
@@ -288,5 +197,12 @@ public class User extends IdEntity{
 			this.userName = userName;
 		}
 		  
+		  public String getCreatedDate() {
+			return createdDate;
+		}
+		  
+		  public void setCreatedDate(String createdDate) {
+			this.createdDate = createdDate;
+		}
 		  
 }
