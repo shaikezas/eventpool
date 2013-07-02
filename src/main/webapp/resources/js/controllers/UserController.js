af2300f0c289d52aabb166f89f18bb5d791ff0fe
ssingana <ssingana@gmail.com>
2013-07-02 16:16:25 +0530

    user changes.

--- src/main/webapp/resources/js/controllers/UserController.js
+++ src/main/webapp/resources/js/controllers/UserController.js
@@ -1,2 +1,23 @@
 var UserController = function($scope, $http) {
+	
+	$scope.userDetails = {};
+	
+    $scope.fetchUserDetails = function() {
+    	$http.get('getuser').success(function(userDetails){
+        	$scope.userDetails = userDetails;
+        });
+        
+      }
+    
+    $scope.updateUser = function() {        	
+        $http.post('updateuser', $scope.userDetails).success(function() {
+       
+        }).error(function() {
+        	alert("User not updated properly check console for errors.");
+        });
+       
+        
+    }
+    
+    $scope.fetchUserDetails();
 }
