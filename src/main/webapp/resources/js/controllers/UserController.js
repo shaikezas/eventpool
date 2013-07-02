a05042b09b88020cd26146cc175adba27c80f517
ssingana <ssingana@gmail.com>
2013-07-02 17:02:29 +0530

    mismatched tag removed.

--- src/main/webapp/resources/js/controllers/UserController.js
+++ src/main/webapp/resources/js/controllers/UserController.js
@@ -3,7 +3,7 @@ var UserController = function($scope, $http) {
 	$scope.userDetails = {};
 	
     $scope.fetchUserDetails = function() {
-    	$http.get('getuser').success(function(userDetails){
+    	$http.get('user').success(function(userDetails){
         	$scope.userDetails = userDetails;
         });
         
@@ -11,7 +11,7 @@ var UserController = function($scope, $http) {
     
     $scope.updateUser = function() {        	
         $http.post('updateuser', $scope.userDetails).success(function() {
-       
+        	$scope.fetchUserDetails();
         }).error(function() {
         	alert("User not updated properly check console for errors.");
         });
