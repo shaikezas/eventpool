3db948bc52f718c85bd85645e6c585210a571113
ramu <ramuenugurthi@gmail.com>
2013-09-21 21:38:05 +0530

    added solr server in properties

--- src/main/java/com/eventpool/web/controller/SearchServer.java
+++ src/main/java/com/eventpool/web/controller/SearchServer.java
@@ -6,6 +6,7 @@ import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.core.CoreContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.xml.sax.SAXException;
 
@@ -19,6 +20,14 @@ public class SearchServer {
 
     //private static HttpSolrServer httpSolrServer = null;
     //private static EmbeddedSolrServer embeddedSolrServer = null;
+	
+	@Value("$EVENT_POOL{solr.server}")
+	private String server ;
+
+	@Value("$EVENT_POOL{solr.port}")
+	private String port;
+
+	
     public SolrServer solrServer = null;
 
     private static final Logger logger = LoggerFactory.getLogger(SearchServer.class);
@@ -32,7 +41,7 @@ public class SearchServer {
     public SearchServer(boolean useHttpSolrServer) throws ParserConfigurationException, SAXException, IOException {
         logger.info("Using httpSolrServer:" + useHttpSolrServer);
         if(useHttpSolrServer){
-            String solrUrl = "http://zeoevent.com:7924/solr";
+            String solrUrl = "http://"+server+":"+port+"/solr";
             solrServer = new HttpSolrServer(solrUrl);
         }else{
             System.setProperty("solr.solr.home", "C:/solr-suggest-4.0-logs/solr");
@@ -47,4 +56,20 @@ public class SearchServer {
         //Thread.sleep(5000l);
         //solrServer.shutdown();
     }
+
+	public String getServer() {
+		return server;
+	}
+
+	public void setServer(String server) {
+		this.server = server;
+	}
+
+	public String getPort() {
+		return port;
+	}
+
+	public void setPort(String port) {
+		this.port = port;
+	}
 }
