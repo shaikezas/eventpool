eb917f2f0ad7a46ff184e43ec49c1167acf80234
Bala <Bala@Bala-HP>
2013-12-14 14:06:51 +0530

    sql refactoring

--- SqlScripts/categoryInsert.sql
+++ SqlScripts/categoryInsert.sql
@@ -1,18 +1,11 @@
 DELETE FROM CATEGORY;
-INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE) VALUES (1,'Education','Education','EDU',null,'Y');
-INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE) VALUES (2,'Entertainment','Entertainment','ENT',null,'Y');
-INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE) VALUES (3,'Fashion','Fashion','FSH',null,'Y');
-INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE) VALUES (4,'Health','Health','HLT',null,'Y');
-INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE) VALUES (5,'Industrial','Industrial','IND',null,'Y');
-INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE) VALUES (6,'RealEstate','RealEstate','RES',null,'Y');
-INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE) VALUES (7,'Sports','Sports','SPR',null,'Y');
-INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE) VALUES (1000,'Miscellaneous','Miscellaneous','MIS',null,'Y');
-
-ALTER TABLE CATEGORY ADD IMAGE_URL varchar(256) NOT NULL;
- UPDATE CATEGORY SET IMAGE_URL = 'eventpool/images/category/1000X300_animal.jpg';
-ALTER TABLE CATEGORY ADD PROMO_LOGO varchar(256) NOT NULL;
-UPDATE CATEGORY SET PROMO_LOGO = 'eventpool/images/category/228X275_animal.jpg';
-COMMIT;
-
+INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE,IMAGE_URL,PROMO_LOGO) VALUES (1,'Education','Education','EDU',null,'Y','eventpool/images/category/1000X300_animal.jpg','eventpool/images/category/228X275_animal.jpg');
+INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE,IMAGE_URL,PROMO_LOGO) VALUES (2,'Entertainment','Entertainment','ENT',null,'Y','eventpool/images/category/1000X300_animal.jpg','eventpool/images/category/228X275_animal.jpg');
+INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE,IMAGE_URL,PROMO_LOGO) VALUES (3,'Fashion','Fashion','FSH',null,'Y','eventpool/images/category/1000X300_animal.jpg','eventpool/images/category/228X275_animal.jpg');
+INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE,IMAGE_URL,PROMO_LOGO) VALUES (4,'Health','Health','HLT',null,'Y','eventpool/images/category/1000X300_animal.jpg','eventpool/images/category/228X275_animal.jpg');
+INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE,IMAGE_URL,PROMO_LOGO) VALUES (5,'Industrial','Industrial','IND',null,'Y','eventpool/images/category/1000X300_animal.jpg','eventpool/images/category/228X275_animal.jpg');
+INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE,IMAGE_URL,PROMO_LOGO) VALUES (6,'RealEstate','RealEstate','RES',null,'Y','eventpool/images/category/1000X300_animal.jpg','eventpool/images/category/228X275_animal.jpg');
+INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE,IMAGE_URL,PROMO_LOGO) VALUES (7,'Sports','Sports','SPR',null,'Y','eventpool/images/category/1000X300_animal.jpg','eventpool/images/category/228X275_animal.jpg');
+INSERT INTO CATEGORY(ID,NAME,DESCRIPTION,CODE,PARENT_CATEGORYID,ACTIVE,IMAGE_URL,PROMO_LOGO) VALUES (1000,'Miscellaneous','Miscellaneous','MIS',null,'Y','eventpool/images/category/1000X300_animal.jpg','eventpool/images/category/228X275_animal.jpg');
 
 
