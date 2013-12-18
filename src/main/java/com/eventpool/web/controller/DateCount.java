bd3507156a2eaf1b1e1b3d844a2a7ac3daaf02f5
ramu <ramuenugurthi@gmail.com>
2013-12-18 23:04:09 +0530

    refactored code

---
+++ src/main/java/com/eventpool/web/controller/DateCount.java
@@ -0,0 +1,12 @@
+package com.eventpool.web.controller;
+
+public class DateCount {
+	
+	public Long todayCount =0L;
+	public Long tomorrowCount=0L;
+	public Long currentWeekCount=0L;
+	public Long nextWeekCount=0L;
+	public Long currentMonthCount=0L;
+	public Long otherDatesCount=0L;
+
+}
